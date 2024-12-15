ace25dcde6da3ddf0a4f015bb8c745e0af50e1a7
Santhosh <71515865+SanthoshKumar9028@users.noreply.github.com>
2024-12-15 17:36:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const URL = require("url").URL;
+let express = require("express");
 const cors = require('cors');
-const app = express();
+let path = require("path");
+let bodyParser = require("body-parser");
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+let app = express();
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const addressMap = new Map();
+
+const stringIsAValidUrl = (s, protocols) => {
+  try {
+    url = new URL(s);
+    return protocols
+      ? url.protocol
+        ? protocols.map((x) => `${x.toLowerCase()}:`).includes(url.protocol)
+        : false
+      : true;
+  } catch (err) {
+    return false;
+  }
+};
+
+app.post("/api/shorturl", (req, res) => {
+  if (stringIsAValidUrl(req.body.url, ["http", "https"])) {
+    const key = addressMap.size + 1;
+    addressMap[key] = req.body.url;
+    const obj = { original_url: req.body.url, short_url: key };
+    console.log(obj);
+
+    res.json(obj);
+  } else {
+    res.json({ error: "invalid url" });
+  }
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/shorturl/:short_url", (req, res) => {
+  console.log(addressMap[+req.params.short_url]);
+
+  if (addressMap[+req.params.short_url]) {
+    res.writeHead(302, { Location: addressMap[+req.params.short_url] });
+    res.end();
+  } else {
+    res.status(404);
+    res.end();
+  }
 });
 
 app.listen(port, function() {
